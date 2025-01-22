172db8abf43311c941c8c2a31f0cf4b2f7dc1fc2
Robert Cambridge <robert@cambridge.me>
2025-01-22 07:02:23 +0100

    update nixpkgs to latest

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   # Add all your dependencies here
   inputs = {
     # pin nixpkgs https://github.com/NixOS/nixpkgs/pull/362081#issuecomment-2596822330
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=7cf092925906d588daabc696d663c100f2bbacc6";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
