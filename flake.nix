a8f15810a9b9d917b9ea9dfd875abf7dc4f7f96f
Robert Cambridge <robert@cambridge.me>
2025-03-26 15:09:27 +0100

    bisect nixpkgs

--- flake.nix
+++ flake.nix
@@ -11,7 +11,8 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs?ref=bff5f93f3673edf9c08670b9ffd3619752e76619";
+    nixpkgs.url = "github:rcambrj/nixpkgs?ref=2da4f42b82b099ed9a1153a9e07efe37ebddc9b6";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
