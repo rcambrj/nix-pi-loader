6cf0a7b6447566247f59e035aebcd5aefadb2fdf
Robert Cambridge <robert@cambridge.me>
2025-03-27 11:41:07 +0100

    revert to working nixpkgs due to #3

--- flake.nix
+++ flake.nix
@@ -11,7 +11,8 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=7cf092925906d588daabc696d663c100f2bbacc6";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
