b4c5618f362a878f52392f77f05e22fd3198ad42
Robert Cambridge <robert@cambridge.me>
2025-03-31 18:08:53 +0200

    make garnix build system images

---
+++ packages/garnix-build.nix
@@ -0,0 +1,25 @@
+#
+# this file exists to make garnix build and cache all of the:
+# nixosConfigurations.*.config.system.build.image
+# because garnix.yml does not permit specifying these targets directly
+#
+{ flake, pkgs, ... }:
+with builtins;
+with pkgs.lib;
+let
+  optionalImage = hostConfig: if (
+    (hasAttr "config" hostConfig) &&
+    (hasAttr "system" hostConfig.config) &&
+    (hasAttr "build"  hostConfig.config.system) &&
+    (hasAttr "image"  hostConfig.config.system.build)
+  ) then hostConfig.config.system.build.image else false;
+  imagesAndFalses = mapAttrs
+    (hostName: hostConfig: optionalImage hostConfig)
+    flake.nixosConfigurations;
+  images = filterAttrs
+    (hostName: hostImage: hostImage != false)
+    imagesAndFalses;
+in traceSeqN 1 (images) pkgs.symlinkJoin {
+  name = "garnix-build";
+  paths = attrValues images;
+}
